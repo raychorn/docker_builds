5f7bafb1df7a5f7999d31b9c3035f46b422e0db8
root <root@DESKTOP-JJ95ENL.localdomain>
2020-10-28 06:54:32 -0600

    added docker routable ip address to container.

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo $USER
-docker run -it --mount src="$(pwd)",target=/test_container,type=bind k3_s3
+docker run -it -p 10.0.0.2:9000:8080 --mount src="$(pwd)",target=/test_container,type=bind k3_s3
